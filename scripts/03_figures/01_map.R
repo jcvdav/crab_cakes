fe2020a6597aff95aff8db15eb09acc9c45a6960
jcvdav <juancarlos.villader@gmail.com>
2026-07-28 09:44:34 -0400

    Updated map

--- scripts/03_figures/01_map.R
+++ scripts/03_figures/01_map.R
@@ -56,65 +56,48 @@ polygons <- vertices %>%
 
 ## Producing referenc ----------------------------------------------------------------
 mexico <- ne_countries(scale = "large", country = "mexico", returnclass = "sf")
-world  <- ne_countries(scale = "large", returnclass = "sf")
+world  <- ne_countries(scale = "small", returnclass = "sf")
 
 ## Reference map  ----------------------------------------------------------------
 label_coordinates <- data.frame(
   letter = c("B", "C"),
-  lon    = c(-115.7, -115.05), 
-  lat    = c(29.85, 27.87)  
+  lon    = c(-115.9, -115.2), 
+  lat    = c(29.9, 27.9)  
 )
 
 label_sf <- st_as_sf(label_coordinates, coords = c("lon", "lat"), crs = 4326)
 
 context_map <- ggplot() +
-  geom_sf(data = mexico, fill = "grey90", color = "grey80", size = 0.3) +
-  geom_sf(data = polygons, fill = "#114b85", color = "#114b85", size = 0.3) +
-  coord_sf(xlim = c(-117, -113), ylim = c(26, 31), expand = FALSE) +
+  geom_sf(data = mexico, fill = "grey90", color = "black", size = 0.3) +
+  coord_sf(xlim = c(-117, -113), ylim = c(26, 31)) +
   labs(x = NULL, y = NULL)+
-  annotation_scale(location = "br", width_hint = 0.3, text_col = "black", bar_cols = "white", text_family = "serif") +
-  theme(
-    text = element_text(size = 12, family = "serif"), 
-    plot.title = element_text(size = 14, face = "bold", color = "black"),
-    axis.title = element_blank(),
-    legend.position = "right",
-    panel.background = element_rect(fill = "#def3fa", color = NA),
-    plot.background  = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    panel.border = element_rect(color = "gray40", fill = NA),
-    axis.text = element_text(color = "gray20"),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  ) + 
-  geom_sf_text(
-    data = label_sf, 
-    aes(label = letter),
-    color = "black",       
-    fontface = "bold",  
-    family = "serif",
-    size = 8              
-  )
+  scale_x_continuous(breaks = -117:-113) +
+  annotation_scale(location = "br",
+                   width_hint = 0.3) +
+  theme_linedraw() +
+  theme(panel.background = element_rect(fill = "#def3fa", color = NA),
+        plot.background  = element_rect(fill = "white", color = NA),
+        panel.border = element_rect(color = "gray40", fill = NA),
+        axis.text = element_text(color = "gray20")) + 
+  geom_sf_text(data = label_sf, 
+               aes(label = letter),
+               color = "black",
+               size = 5)
 
 
 ### Inset map 
 box <- st_as_sfc(st_bbox(polygons))
 
 inset_map <- ggplot() +
-  geom_sf(data = mexico, fill = "grey80", color = "grey80", size = 0.3) +
-  geom_sf(data = world, fill = "gray95", color = "grey80", size = 0.3) +
-  geom_sf(data = box, fill = NA, color = "red", size = 1.2) +
-  coord_sf(xlim = c(-120, -86), ylim = c(13, 34), expand = FALSE) +
+  geom_sf(data = world, fill = "gray95", color = "black", size = 0.3) +
+  geom_sf(data = box, fill = NA, color = "red", size = 0.5) +
+  coord_sf(xlim = c(-120, -86), ylim = c(13, 34)) +
   labs(x = NULL, y = NULL)+
-  theme(
-    text = element_text(size = 12, family = "serif"), 
-    plot.title = element_text(size = 14, face = "bold", color = "black"),
-    axis.title = element_blank(),
-    panel.background = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    plot.background = element_blank(),
-    panel.border = element_rect(color = "gray40", fill = NA),
-    axis.ticks = element_blank(),
-    axis.text = element_blank()
-  ) 
+  theme_void() +
+  theme(panel.background = element_rect(fill = "white", color = NA),
+        panel.grid.major = element_blank(),
+        plot.background = element_blank(),
+        panel.border = element_rect(color = "black", fill = NA)) 
 
 map_with_inset <- ggdraw() +
   draw_plot(context_map) +
@@ -124,65 +107,51 @@ map_with_inset <- ggdraw() +
  
 ### El Rosario
 elrosario <- ggplot() +
-  geom_sf(data = mexico, fill = "grey90", color = "grey80", size = 0.3) +
+  geom_sf(data = mexico, fill = "grey90", color = "black", size = 0.3) +
   geom_sf(data = polygons, fill = "#114b85", color = "#114b85", size = 0.3) +
-  coord_sf(xlim = c(-115.9, -115.5), ylim = c(29.7, 30.05), expand = FALSE) +
-  labs(title = "El Rosario", x = NULL, y = NULL)+
-  annotation_scale(location = "bl", width_hint = 0.3, text_col = "black", bar_cols = "white", text_family = "serif") +
-  theme(
-    text = element_text(size = 12, family = "serif"), 
-    plot.title = element_text(size = 14, face = "bold", color = "black"),
-    axis.title = element_blank(),
-    legend.position = "right",
-    panel.background = element_rect(fill = "#def3fa", color = NA),
-    plot.background  = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    panel.border = element_rect(color = "gray40", fill = NA),
-    axis.text = element_text(color = "gray20"),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  ) 
+  coord_sf(xlim = c(-115.9, -115.5), ylim = c(29.7, 30.05)) +
+  scale_x_continuous(breaks = seq(-115.9, -115.5, by = 0.1)) +
+  scale_y_continuous(breaks = seq(29.7, 30.05, by = 0.1)) +
+  annotation_scale(location = "bl") +
+  theme_linedraw() +
+  theme(panel.background = element_rect(fill = "#def3fa", color = NA),
+        plot.background  = element_rect(fill = "white", color = NA),
+        panel.border = element_rect(color = "gray40", fill = NA),
+        axis.text = element_text(color = "gray20")) 
 
 ### Isla Natividad
 islanatividad <- ggplot() +
-  geom_sf(data = mexico, fill = "grey90", color = "grey80", size = 0.3) +
+  geom_sf(data = mexico, fill = "grey90", color = "black", size = 0.3) +
   geom_sf(data = polygons, fill = "#114b85", color = "#114b85", size = 0.3) +
   coord_sf(xlim = c(-115.3, -114.9), ylim = c(27.75, 28.1), expand = FALSE) +
-  labs(title = "Isla Natividad", x = NULL, y = NULL)+
-  annotation_scale(location = "bl", width_hint = 0.3, text_col = "black", bar_cols = "white", text_family = "serif") +
-  theme(
-    text = element_text(size = 12, family = "serif"), 
-    plot.title = element_text(size = 14, face = "bold", color = "black"),
-    axis.title = element_blank(),
-    legend.position = "right",
-    panel.background = element_rect(fill = "#def3fa", color = NA),
-    plot.background  = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    panel.border = element_rect(color = "gray40", fill = NA),
-    axis.text = element_text(color = "gray20"),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  ) 
+  scale_x_continuous(breaks = seq(-115.3, -114.9, by = 0.2)) +
+  scale_y_continuous(breaks = seq(27.75, 28.1, by = 0.1)) +
+  annotation_scale(location = "bl") +
+  theme_linedraw() +
+  theme(panel.background = element_rect(fill = "#def3fa", color = NA),
+        plot.background  = element_rect(fill = "white", color = NA),
+        panel.border = element_rect(color = "gray40", fill = NA),
+        axis.text = element_text(color = "gray20")) 
 
 ## Combining map layouts  ----------------------------------------------------------------
-community_plots <- (elrosario / islanatividad)
+community_plots <- plot_grid(elrosario, 
+                             islanatividad, 
+                             ncol = 1, 
+                             labels = c("B", "C"),
+                             align = "hv")
 
-map_layout <- wrap_elements(map_with_inset) | community_plots + 
-  plot_layout(widths = c(2, 1))
-
-final_map <- map_layout + 
-  plot_annotation(
-    tag_levels = list(c("A", "B", "C")), 
-    title = "Location of community based marine reserves open to fishing", 
-    theme = theme( 
-      plot.title = element_text(size = 18, face = "bold", hjust = 0.5, family = "serif"),
-      plot.tag = element_text(size = 14, face = "bold", family = "serif"),
-  #    plot.margin = margin(10, 10, 5, 10),
-      text = element_text(family = "serif")
-    )
-  )
+map_layout <- plot_grid(map_with_inset, 
+                        community_plots,
+                        ncol = 2,
+                        rel_widths = c(1.25, 1),
+                        labels = c("A", ""))
 
 # EXPORT #######################################################################
 
 ## Export to a file called map.png using a 9X6 aspect ratio --------------------
   
-ggsave(here("results", "figs", "map.png"), plot = final_map, width = 9, height = 6)
+ggsave(plot = map_layout,
+       filename = here("results", "figs", "map.png"),bg = "white",
+       width = 9,
+       height = 6)
 
